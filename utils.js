73868d1028af4facf47a31d25e221d336315dbba
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-04-17 21:38:32 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,15 +1,12 @@
-// utils.js
-function getRandomPosition() {
-  const container = document.getElementById('game-container');
+import { container, videoOverlay } from './game.js';
+
+export function getRandomPosition() {
   const rect = container.getBoundingClientRect();
   const x = Math.random() * (rect.width - 50);
   const y = Math.random() * (rect.height - 50);
   return { x, y };
 }
 
-function isVideoPlaying() {
-  const videoOverlay = document.getElementById('video-overlay');
+export function isVideoPlaying() {
   return videoOverlay.style.display === 'flex';
 }
-
-export { getRandomPosition, isVideoPlaying };
