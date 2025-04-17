d5d628681110d4fe18c275ba170e6015b5b8cacb
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-04-18 01:13:01 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,12 +1,15 @@
-import { container, videoOverlay } from './game.js';
-
-export function getRandomPosition() {
-  const rect = container.getBoundingClientRect();
-  const x = Math.random() * (rect.width - 50);
-  const y = Math.random() * (rect.height - 50);
-  return { x, y };
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-export function isVideoPlaying() {
-  return videoOverlay.style.display === 'flex';
+export function isColliding(a, b) {
+  const rect1 = a.getBoundingClientRect();
+  const rect2 = b.getBoundingClientRect();
+
+  return !(
+    rect1.right < rect2.left ||
+    rect1.left > rect2.right ||
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom
+  );
 }
