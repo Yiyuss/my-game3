cd16f79336a9843538738a22b14d72c6ff0cf5b9
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-04-18 01:30:00 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,15 +1,1 @@
-export function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
 
-export function isColliding(a, b) {
-  const rect1 = a.getBoundingClientRect();
-  const rect2 = b.getBoundingClientRect();
-
-  return !(
-    rect1.right < rect2.left ||
-    rect1.left > rect2.right ||
-    rect1.bottom < rect2.top ||
-    rect1.top > rect2.bottom
-  );
-}
